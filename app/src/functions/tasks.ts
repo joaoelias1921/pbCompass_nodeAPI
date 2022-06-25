9c08a4c538dbb0afebc80754d1f8595236603bc7
igBFranco <igor.franco@aluno.unc.br>
2022-06-25 14:40:13 -0300

    Adding Tasks POST on FrontEND

--- app/src/functions/tasks.ts
+++ app/src/functions/tasks.ts
@@ -11,7 +11,7 @@ function fetchData() {
 
 function listTasks(tasks){
 return tasks.map(function(task) {
-    const ul = document.getElementById('tasks')
+    const ul = document.getElementById('tasks')!;
     let li = document.createElement('li');
     let description = document.createElement('p');
     let user = document.createElement('p');
@@ -29,5 +29,26 @@ return tasks.map(function(task) {
 
 
 function addTask() {
-console.log('teste');
+    const description = document.getElementById('description')!;
+    const datetime = document.getElementById('time')!;
+    const user = document.getElementById('user')!;
+
+    let data = {
+        description: `${description.value}`, 
+        date: `${datetime.value}`, 
+        user: `${user.value}`
+    }
+
+    fetch('http://localhost:3000/tasks', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json);
+    });
+    
 }
